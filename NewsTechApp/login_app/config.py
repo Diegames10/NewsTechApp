a967837bbfe8a686f66edb9fb68414c5dea50866
Diegames10 <diegoandrioli_48@hotmail.com>
2025-08-24 01:09:13 -0300

    Update config.py

--- NewsTechApp/login_app/config.py
+++ NewsTechApp/login_app/config.py
@@ -1,1 +1,4 @@
-# Configurações podem ser movidas para cá futuramente
+class Config:
+    SECRET_KEY = "uma_chave_secreta_aqui"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///app.db"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
