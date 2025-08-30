cd4dfd98c29bdd0f06b09f4ed3ab21b960a42684
Diegames10 <diegoandrioli_48@hotmail.com>
2025-08-30 00:35:36 -0300

    Update app.py

--- NewsTechApp/login_app/app.py
+++ NewsTechApp/login_app/app.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_dance.contrib.google import make_google_blueprint
