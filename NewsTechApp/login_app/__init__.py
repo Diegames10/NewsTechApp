4d1989d8e5acdcf50827477c6c8905243f0387f1
Diegames10 <diegoandrioli_48@hotmail.com>
2025-08-30 05:14:45 -0300

    Update __init__.py

--- NewsTechApp/login_app/__init__.py
+++ NewsTechApp/login_app/__init__.py
@@ -1,21 +1,18 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
-
-
-db = SQLAlchemy()
-bcrypt = Bcrypt()
+from .config import Config
+from .extensions import db, bcrypt, migrate
+from .routes.auth import auth_bp, google_bp, github_bp
 
 def create_app():
     app = Flask(__name__)
     
-    app.config.from_object("login_app.config.Config")
+    app.config.from_object(Config)  # Carrega as configurações
     
     db.init_app(app)
     bcrypt.init_app(app)
+    migrate.init_app(app, db)
 
     # Registrar blueprints
-    from login_app.routes.auth import auth_bp, google_bp, github_bp
     app.register_blueprint(auth_bp)
     app.register_blueprint(google_bp, url_prefix="/login/google")
     app.register_blueprint(github_bp, url_prefix="/login/github")
