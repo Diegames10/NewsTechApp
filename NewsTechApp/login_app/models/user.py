7a2d09c9c50eafe01f0aeefd5aaf6f19551e61b7
Diegames10 <diegoandrioli_48@hotmail.com>
2025-08-30 01:20:26 -0300

    Update user.py

--- NewsTechApp/login_app/models/user.py
+++ NewsTechApp/login_app/models/user.py
@@ -1,14 +1,11 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
-
-db = SQLAlchemy()
-bcrypt = Bcrypt()
+from app import db
+from flask_bcrypt import generate_password_hash
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
-    password_hash = db.Column(db.String(200), nullable=False)
-    provider = db.Column(db.String(50), default="local")
+    password_hash = db.Column(db.String(255), nullable=False)
+    provider = db.Column(db.String(50), nullable=False, default="local")
 
-    def __repr__(self):
-        return f"<User {self.username}>"
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password).decode('utf-8')
