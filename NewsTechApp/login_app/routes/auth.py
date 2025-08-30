910016291dcc3f55cf413cc6e3925f4af7cb7539
Diegames10 <diegoandrioli_48@hotmail.com>
2025-08-30 04:54:28 -0300

    Update auth.py

--- NewsTechApp/login_app/routes/auth.py
+++ NewsTechApp/login_app/routes/auth.py
@@ -38,7 +38,7 @@ def login():
         password = request.form["password"]
 
         user = User.query.filter_by(username=username).first()
-        if user and bcrypt.check_password_hash(user.password, password):
+        if user and bcrypt.check_password_hash(user.password_hash, password):
             session["user_id"] = user.id
             flash("Login realizado com sucesso!", "success")
             return redirect(url_for("auth.dashboard"))  # <- corrigido
@@ -62,7 +62,7 @@ def register():
 
         # Cria novo usuário
         hashed_password = bcrypt.generate_password_hash(password).decode("utf-8")
-        new_user = User(username=username, password=hashed_password)
+        new_user = User(username=username, password_hash=hashed_password)
         db.session.add(new_user)
         db.session.commit()
 
