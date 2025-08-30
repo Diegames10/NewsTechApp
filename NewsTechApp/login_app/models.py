a798cc3f7aa378469976a12eb501ca1ab3dc8c2f
Diegames10 <diegoandrioli_48@hotmail.com>
2025-08-29 23:46:10 -0300

    Delete NewsTechApp/login_app/models.py
    desnecessário

--- NewsTechApp/login_app/models.py
+++
@@ -1,7 +0,0 @@
-from login_app import db
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(150), unique=True, nullable=False)
-    password_hash = db.Column(db.String(255), nullable=False)
-    provider = db.Column(db.String(50), nullable=False, default="local")
