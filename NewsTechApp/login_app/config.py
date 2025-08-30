6008835959f6939e729aa6721d8879c49d4e2138
Diegames10 <diegoandrioli_48@hotmail.com>
2025-08-30 05:12:18 -0300

    Update config.py

--- NewsTechApp/login_app/config.py
+++ NewsTechApp/login_app/config.py
@@ -1,4 +1,10 @@
+import os
+
 class Config:
-    SECRET_KEY = "uma_chave_secreta_aqui"
-    SQLALCHEMY_DATABASE_URI = "sqlite:////data/app.db"  # caminho absoluto no disco persistente
+    SECRET_KEY = os.environ.get("SECRET_KEY", "supersecret")  # Defina uma chave secreta segura para produção
+    SQLALCHEMY_DATABASE_URI = f"sqlite:////data/app.db"  # Caminho para o banco de dados
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
+    GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
+    GITHUB_CLIENT_ID = os.getenv("GITHUB_CLIENT_ID")
+    GITHUB_CLIENT_SECRET = os.getenv("GITHUB_CLIENT_SECRET")
