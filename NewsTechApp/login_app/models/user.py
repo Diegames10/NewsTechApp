5e8ceb7c76dd26dca3999b969a611b98ad6cb2fc
Diegames10 <diegoandrioli_48@hotmail.com>
2025-10-12 16:22:23 -0300

    Update user.py

--- NewsTechApp/login_app/models/user.py
+++ NewsTechApp/login_app/models/user.py
@@ -1,20 +1,14 @@
-from login_app.extensions import db, bcrypt
+from login_app import db
+from flask_bcrypt import generate_password_hash, check_password_hash
 
 class User(db.Model):
-    __tablename__ = 'user'  # opcional, só para deixar explícito
-
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), unique=True, nullable=False)
+    username = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(128))
 
-    email = db.Column(db.String(150), unique=True, nullable=True)
-    
     def set_password(self, password):
-        self.password_hash = bcrypt.generate_password_hash(password).decode("utf-8")
+        self.password_hash = generate_password_hash(password).decode('utf-8')
 
     def check_password(self, password):
-        return bcrypt.check_password_hash(self.password_hash, password)
-
-    def __repr__(self):
-        return f"<User {self.username}>"
+        return check_password_hash(self.password_hash, password)
