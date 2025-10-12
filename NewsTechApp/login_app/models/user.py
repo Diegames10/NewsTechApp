b940cdf3edc8029d006e5c947e34c60c5fecc6c8
Diegames10 <diegoandrioli_48@hotmail.com>
2025-10-11 22:59:49 -0300

    Update user.py

--- NewsTechApp/login_app/models/user.py
+++ NewsTechApp/login_app/models/user.py
@@ -8,6 +8,8 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
 
+    email = db.Column(db.String(150), unique=True, nullable=True)
+    
     def set_password(self, password):
         self.password_hash = bcrypt.generate_password_hash(password).decode("utf-8")
 
