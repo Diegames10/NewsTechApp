927caa213659db8462df117fc11de484e2e81f29
Diegames10 <diegoandrioli_48@hotmail.com>
2025-10-12 16:23:50 -0300

    V.3.0.2 __init__.py

--- NewsTechApp/login_app/__init__.py
+++ NewsTechApp/login_app/__init__.py
@@ -1,11 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
-from werkzeug.middleware.proxy_fix import ProxyFix
-import os
+from flask_migrate import Migrate
 
-# Extensões globais
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 migrate = Migrate()
@@ -13,23 +10,15 @@ migrate = Migrate()
 def create_app():
     app = Flask(__name__)
 
-    # Corrige HTTPS no Render
-    app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
+    app.config.from_object("login_app.config.Config")
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////data/app.db"
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "changeme")
-
-    # Inicializa extensões
     db.init_app(app)
     bcrypt.init_app(app)
     migrate.init_app(app, db)
 
-    # Importa e registra Blueprints
     from login_app.routes.auth import auth_bp, google_bp, github_bp
     app.register_blueprint(auth_bp)
     app.register_blueprint(google_bp, url_prefix="/oauth2/login")
     app.register_blueprint(github_bp, url_prefix="/oauth2/login")
-
 
     return app
