c66944974bbd9a471f8e764ff8c25546734d89e8
Diegames10 <diegoandrioli_48@hotmail.com>
2025-10-12 16:18:35 -0300

    Update __init__.py

--- NewsTechApp/login_app/__init__.py
+++ NewsTechApp/login_app/__init__.py
@@ -28,7 +28,8 @@ def create_app():
     # Importa e registra Blueprints
     from login_app.routes.auth import auth_bp, google_bp, github_bp
     app.register_blueprint(auth_bp)
-    app.register_blueprint(google_bp, url_prefix="/oauth2/login/google")
-    app.register_blueprint(github_bp, url_prefix="/oauth2/login/github")
+    app.register_blueprint(google_bp, url_prefix="/oauth2/login")
+    app.register_blueprint(github_bp, url_prefix="/oauth2/login")
+
 
     return app
