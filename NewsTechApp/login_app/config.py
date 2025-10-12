619a1c9dadae77ec0196583b96743031dd3b4bf0
Diegames10 <diegoandrioli_48@hotmail.com>
2025-10-12 18:30:59 -0300

    Update config.py

--- NewsTechApp/login_app/config.py
+++ NewsTechApp/login_app/config.py
@@ -3,10 +3,4 @@ class Config:
     SQLALCHEMY_DATABASE_URI = "sqlite:////data/app.db"  # caminho absoluto no disco persistente
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    # Configuração de e-mail (Brevo)
-    MAIL_SERVER = os.getenv("MAIL_SERVER", "smtp-relay.brevo.com")
-    MAIL_PORT = int(os.getenv("MAIL_PORT", 587))
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.getenv("MAIL_USERNAME")
-    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
-    MAIL_DEFAULT_SENDER = os.getenv("MAIL_DEFAULT_SENDER", "NewsTechApp <no-reply@newstechapp.com>")
+   
