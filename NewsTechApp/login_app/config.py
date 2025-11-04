efea67fdf7a55211c9c80d1a15963974f5a1847a
Diegames10 <diegoandrioli_48@hotmail.com>
2025-11-04 09:35:16 -0300

    Update config.py

--- NewsTechApp/login_app/config.py
+++ NewsTechApp/login_app/config.py
@@ -1,6 +1,15 @@
+import os
+
 class Config:
     SECRET_KEY = "uma_chave_secreta_aqui"
     SQLALCHEMY_DATABASE_URI = "sqlite:////data/app.db"  # caminho absoluto no disco persistente
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-   
+    # Novo bind para postagens
+    POSTS_DATABASE_URI = os.environ.get(
+        "POSTS_DATABASE_URI", "sqlite:////data/posts.db"
+    )
+    SQLALCHEMY_BINDS = {
+        "posts": POSTS_DATABASE_URI,
+    }
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
