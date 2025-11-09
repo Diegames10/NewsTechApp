701b17c03e7eed3ea99a9730ee412f70a1e62a3c
Diegames10 <diegoandrioli_48@hotmail.com>
2025-11-08 21:41:29 -0300

    Delete extensions.py

--- extensions.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
-from flask_migrate import Migrate
-from flask_mail import Mail
-
-db = SQLAlchemy()
-bcrypt = Bcrypt()
-migrate = Migrate()
-mail = Mail()
